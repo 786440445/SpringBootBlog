0a8dbbdfde172da9441c0d0d5f86bc4c1314db10
Chengli <786440445@qq.com>
2018-12-30 04:10:32 +0800

    modify some html file

--- src/main/java/com/wip/utils/Tools.java
+++ src/main/java/com/wip/utils/Tools.java
@@ -1,10 +1,8 @@
 package com.wip.utils;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 import java.util.Random;
 
 public class Tools {
@@ -17,14 +15,16 @@ public class Tools {
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"),"ASE");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
         byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-        return new BASE64Encoder().encode(encryptedBytes);
+        Base64.Encoder encoder = Base64.getEncoder();
+        return encoder.encodeToString(encryptedBytes);
     }
 
     public static String deAes(String data, String key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        byte[] cipherTextBytes = new BASE64Decoder().decodeBuffer(data);
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] cipherTextBytes = decoder.decode(data);
         byte[] decValue = cipher.doFinal(cipherTextBytes);
         return new String(decValue);
     }
